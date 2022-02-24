a1d7f180742a261441d550191002a7d10210adc0
LosyashBOI <82841870+LosyashBOI@users.noreply.github.com>
2022-02-24 19:34:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+import {BUTTON} from './view.js';
+
+function addTask(event) {
+    let taskList = event.currentTarget.parentElement.nextElementSibling;
+    let input = event.currentTarget.previousElementSibling.value;
+
+    if (input.trim()) {
+        let newTask = document.createElement('li');
+        
+        newTask.className = 'to-do__list-item flex';
+        newTask.innerHTML = `<button class="checkbox-btn"></button> <p class="to-do__task-descr">${input}</p> <button class="del-task-btn"></button>`;
+        taskList.prepend(newTask);
+
+        newTask.lastElementChild.addEventListener('click', deleteTask);
+        newTask.firstElementChild.addEventListener('click', changeStatus);
+    }
+}
+
+function deleteTask(event) {
+    event.currentTarget.parentElement.remove();
+}
+
+function changeStatus(event) {
+    event.currentTarget.classList.toggle('checkbox-btn_done');
+    event.currentTarget.parentElement.classList.toggle('to-do__list-item_done');
+}
+
+for (let btn of BUTTON.ADD_TASK) {
+    btn.addEventListener('click', addTask);
+}
+
+for (let btn of BUTTON.DELETE_TASK) {
+    btn.addEventListener('click', deleteTask);
+}
+
+for (let btn of BUTTON.CHANGE_STATUS) {
+    btn.addEventListener('click', changeStatus);
+}
+
---
+++ view.js
@@ -0,0 +1,5 @@
+export const BUTTON = {
+    ADD_TASK: document.querySelectorAll('.form__input-btn'),
+    DELETE_TASK: document.querySelectorAll('.del-task-btn'),
+    CHANGE_STATUS: document.querySelectorAll('.checkbox-btn'),
+};
